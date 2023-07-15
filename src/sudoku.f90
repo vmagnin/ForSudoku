70353e22e8ec20363566f38fa8884b01a5be9feb
vmagnin <vincent.magnin@libertysurf.fr>
2023-07-15 10:51:18 +0200

    More spaces

--- src/sudoku.f90
+++ src/sudoku.f90
@@ -37,21 +37,21 @@ contains
         integer(1), dimension(1:9)      :: chiffrePossible    ! Liste des chiffres possibles
         integer(1) :: compteurCP    ! Compteur de chiffres possibles
 
-        chiffrePossible=0
+        chiffrePossible = 0
 
         ! Sauvegarde de la grille de départ :
-        g0=g
+        g0 = g
 
         ! Parcourir la grille, repérer les cases vides et stocker leurs coordonnées
         ! dans un tableau de 81 éléments.
-        casesVides=0
-        compteurCV=0
-        do l=1,9,+1
-            do c=1,9,+1
-                if (g(l,c)==0) then
-                    compteurCV=compteurCV+1
-                    casesVides(compteurCV,1)=l
-                    casesVides(compteurCV,2)=c
+        casesVides = 0
+        compteurCV = 0
+        do l = 1,9,+1
+            do c = 1,9,+1
+                if (g(l,c) == 0) then
+                    compteurCV = compteurCV+1
+                    casesVides(compteurCV,1) = l
+                    casesVides(compteurCV,2) = c
                     !call lister_chiffres_possibles(g,l,c,casesVides(compteurCV,3),chiffrePossible)
                 end if
             end do
@@ -61,39 +61,39 @@ contains
         !call Trier(casesVides,1,compteurCV)
 
         ! Parcourir l'ensemble des cases vides.
-        i=1
-        do while (i<=compteurCV)
+        i = 1
+        do while (i <= compteurCV)
             ! Afin d'accélèrer l'algorithme, a chaque fois,
             ! on recompte le nombre de chiffres possibles pour
             ! chaque case vide restante :
-            do j=i,compteurCV,+1
-                l0=casesVides(j,1)
-                c0=casesVides(j,2)
+            do j = i,compteurCV,+1
+                l0 = casesVides(j,1)
+                c0 = casesVides(j,2)
                 call lister_chiffres_possibles(g,l0,c0,casesVides(j,3),chiffrePossible)
             end do
             ! Et on retrie les cases vides en fonction du nombre de chiffres possibles
             call Trier(casesVides,i,compteurCV)
 
             ! Etablir la liste des chiffres possibles dans la case vide l0,c0 :
-            l0=casesVides(i,1)
-            c0=casesVides(i,2)
+            l0 = casesVides(i,1)
+            c0 = casesVides(i,2)
 
             call lister_chiffres_possibles(g,l0,c0,compteurCP,chiffrePossible)
 
             ! S'il y en a plusieurs, en tirer un au hasard et passer à la case vide suivante :
-            if (compteurCP>1) then
+            if (compteurCP > 1) then
                 call Random_number(alea)
-                j=1+int(alea*compteurCP)
-                g(l0,c0)=chiffrePossible(j)
-                i=i+1
+                j = 1+int(alea*compteurCP)
+                g(l0,c0) = chiffrePossible(j)
+                i = i+1
             ! S'il n'y en a qu'un, le choisir et passer à la case vide suivante :
-            else if (compteurCP==1) then
-                g(l0,c0)=chiffrePossible(1)
-                i=i+1
+            else if (compteurCP == 1) then
+                g(l0,c0) = chiffrePossible(1)
+                i = i+1
             ! S'il n'y en a pas, on recommence tout :
             else
-                i=1
-                g=g0
+                i = 1
+                g = g0
             end if
         end do
     end subroutine ResoudreGrille
@@ -111,26 +111,26 @@ contains
         integer(1) :: l,c,cr,lr,j
         logical(1), dimension(0:9) :: possible    ! Possibilité de chaque chiffre
 
-        possible=.true.
-        do j=1,9,+1
-            possible(g(j,c0))=.false.
-            possible(g(l0,j))=.false.
+        possible = .true.
+        do j = 1,9,+1
+            possible(g(j,c0)) = .false.
+            possible(g(l0,j)) = .false.
         end do
 
-        lr=1+3*((l0-1)/3)
-        cr=1+3*((c0-1)/3)
-        do l=lr,lr+2,+1
-            do c=cr,cr+2,+1
-                possible(g(l,c))=.false.
+        lr = 1+3*((l0-1)/3)
+        cr = 1+3*((c0-1)/3)
+        do l = lr,lr+2,+1
+            do c = cr,cr+2,+1
+                possible(g(l,c)) = .false.
             end do
         end do
 
-        compteurCP=0
-        chiffrePossible=0
-        do j=1,9,+1
+        compteurCP = 0
+        chiffrePossible = 0
+        do j = 1,9,+1
             if (possible(j)) then
-                compteurCP=compteurCP+1
-                chiffrePossible(compteurCP)=j
+                compteurCP = compteurCP+1
+                chiffrePossible(compteurCP) = j
             end if
         end do
     end subroutine
@@ -152,18 +152,18 @@ contains
         integer(1), dimension(1:3) :: c    ! Sauvegarde
         logical :: fini
 
-        fini=.false.
+        fini = .false.
         do while (.not.fini)
-            fini=.true.
+            fini = .true.
             ! On compare chaque case avec la suivante :
-            do i=p, n-1, +1
-                j=i+1
-                if (casesVides(i,3)>casesVides(j,3)) then
+            do i = p, n-1, +1
+                j = i+1
+                if (casesVides(i,3) > casesVides(j,3)) then
                     ! On echange les deux cases dans la liste :
-                    c=casesVides(i,:)
-                    casesVides(i,:)=casesVides(j,:)
-                    casesVides(j,:)=c
-                    fini=.false.
+                    c = casesVides(i,:)
+                    casesVides(i,:) = casesVides(j,:)
+                    casesVides(j,:) = c
+                    fini = .false.
                 end if
             end do
         end do
@@ -182,31 +182,31 @@ contains
         integer(4) :: essais
         logical(1) :: fini
 
-        g=0
+        g = 0
 
-        l=1
-        do while(l<=9)
-            c=1
-            do while(c<=9)
-                essais=0
-                fini=.false.
+        l = 1
+        do while(l <= 9)
+            c = 1
+            do while(c <= 9)
+                essais = 0
+                fini = .false.
                 do while(.not.fini)
-                    if (essais>30) then
+                    if (essais > 30) then
                         ! On recommence depuis le départ
                         ! (on ne sait pas jusqu'à revenir en arrière)
-                        g=0
-                        essais=0
-                        c=1
-                        l=1
+                        g = 0
+                        essais = 0
+                        c = 1
+                        l = 1
                     end if
-                    essais=essais+1
+                    essais = essais+1
                     call Random_number(alea)
-                    g(l,c)=1+int(alea*9d0)
-                    fini=ChiffreValide(g,l,c)
+                    g(l,c) = 1+int(alea*9d0)
+                    fini = ChiffreValide(g,l,c)
                 end do
-                c=c+1
+                c = c+1
             end do
-            l=l+1
+            l = l+1
         end do
     end subroutine GenererGrillePleine
 
@@ -218,10 +218,11 @@ contains
         ! Variables locales
         integer(1) :: i,j
 
-        i=(l-1)/3
-        j=(c-1)/3
+        i = (l-1)/3
+        j = (c-1)/3
 
-        ChiffreValide=ColonneOuLigneValide(g(l,1:9)).and.ColonneOuLigneValide(g(1:9,c)).and.RegionValide(g(i*3+1:i*3+3,j*3+1:j*3+3))
+        ChiffreValide = ColonneOuLigneValide(g(l,1:9)).and.ColonneOuLigneValide(g(1:9,c)) &
+            & .and.RegionValide(g(i*3+1:i*3+3,j*3+1:j*3+3))
     end function ChiffreValide
 
 
@@ -233,7 +234,7 @@ contains
         ! Parametre d'entree :
         integer(1),intent(in) :: restant
         ! Variables locales
-        integer(1), parameter                :: n=10
+        integer(1), parameter                :: n = 10
         integer(1), dimension(1:9, 1:9)      :: g0
         integer(1), dimension(1:n, 1:9, 1:9) :: solutions
         real(8)    :: alea
@@ -241,48 +242,48 @@ contains
         logical(1) :: vide,unique
 
         ! Sauvegarde de la grille de départ :
-        g0=g
+        g0 = g
 
-        unique=.false.
+        unique = .false.
         do while(.not.unique)
-            g=g0
+            g = g0
 
             ! On enlève au hasard les cases vides
-            do i=1, 81-restant
+            do i = 1, 81-restant
                     ! On cherche une case à effacer :
-                    vide=.false.
+                    vide = .false.
                     do while(.not.vide)
                         call Random_number(alea)
-                        l=1+int(alea*9d0)
+                        l = 1+int(alea*9d0)
                         call Random_number(alea)
-                        c=1+int(alea*9d0)
-                        if (g(l,c)/=0) then
-                            vide=.true.
+                        c = 1+int(alea*9d0)
+                        if (g(l,c) /= 0) then
+                            vide = .true.
                         end if
                     end do
                     ! Et on la vide :
-                    g(l,c)=0
+                    g(l,c) = 0
             end do
 
             print *,"Je cherche une grille avec une solution unique..."
 
             ! On résout n fois la grille
-            unique=.true.
-            i=1
-    sol :        do while((i<=n).and.unique)
-                solutions(i,1:9,1:9)=g
+            unique = .true.
+            i = 1
+    sol :        do while((i <= n).and.unique)
+                solutions(i,1:9,1:9) = g
                 call ResoudreGrille(solutions(i,1:9,1:9))
-                if (i>=2) then
-                    do l=1,9,+1
-                        do c=1,9,+1
-                            if (solutions(i,l,c)/=solutions(i-1,l,c)) then
-                                unique=.false.
+                if (i >= 2) then
+                    do l = 1,9,+1
+                        do c = 1,9,+1
+                            if (solutions(i,l,c) /= solutions(i-1,l,c)) then
+                                unique = .false.
                                 EXIT sol
                             end if
                         end do
                     end do
                 end if
-                i=i+1
+                i = i+1
             end do sol
 
             ! Si les n solutions sont identiques, la solution est
@@ -300,9 +301,9 @@ contains
         ! Creation du fichier :
         open(unit=1, file=nom_fichier, STATUS="REPLACE")
 
-        do l=1, 9, +1
+        do l = 1, 9, +1
             write(1,'(i2,i2,i2," |",i2,i2,i2," |",i2,i2,i2)') (g(l,c) , c=1,9)
-            if ((l==3).or.(l==6)) then
+            if ((l == 3).or.(l == 6)) then
                 write(1,*) "------+-------+------"
             end if
         end do
@@ -323,13 +324,13 @@ contains
         ! Ouverture et lecture du fichier ligne par ligne :
         open(unit=1, file=nom_fichier)
 
-        do l=1, 9, +1
+        do l = 1, 9, +1
             READ(1,'(i2,i2,i2,a2,i2,i2,i2,a2,i2,i2,i2)') &
                 & g(l,1),g(l,2),g(l,3), barre1,g(l,4),g(l,5),g(l,6), &
                 & barre2,g(l,7),g(l,8),g(l,9)
 
             ! On saute les lignes de tirets :
-            if ((l==3).or.(l==6)) then
+            if ((l == 3).or.(l == 6)) then
                 READ(1,*)
             end if
         end do
@@ -342,9 +343,9 @@ contains
         integer(1), dimension(1:9, 1:9) :: g
         integer(1) :: l,c    !Numeros lignes et colonnes
 
-        do l=1, 9, +1
+        do l = 1, 9, +1
             print '(i2,i2,i2," |",i2,i2,i2," |",i2,i2,i2)', (g(l,c) , c=1,9)
-            if ((l==3).or.(l==6)) then
+            if ((l == 3).or.(l == 6)) then
                 print *, "------+-------+------"
             end if
         end do
@@ -357,7 +358,7 @@ contains
         ! Variables locales :
         integer(1) :: l,c    !Numeros lignes et colonnes
 
-        do l=1, 9, +1
+        do l = 1, 9, +1
             print *, "Entrez la ligne ",l
             READ *, (g(l,c) , c=1,9)
         end do
@@ -371,12 +372,12 @@ contains
         integer(1), dimension(0:9) :: compteur    !Nb d'apparitions de chaque chiffre
         integer(1) :: l        !Compteur de boucle
 
-        ColonneOuLigneValide=.true.
-        compteur=0
-        do l=1,9,+1
-            compteur(col(l))=compteur(col(l))+1
-            if ((compteur(col(l))>1).and.(col(l)/=0)) then
-                ColonneOuLigneValide=.false.
+        ColonneOuLigneValide = .true.
+        compteur = 0
+        do l = 1,9,+1
+            compteur(col(l)) = compteur(col(l))+1
+            if ((compteur(col(l)) > 1).and.(col(l) /= 0)) then
+                ColonneOuLigneValide = .false.
                 return        !On quitte la fonction
             end if
         end do
@@ -388,19 +389,19 @@ contains
         integer(1), dimension(1:3, 1:3) :: region
         integer(1), dimension(1:9)      :: col
 
-        col(1)=region(1,1)
-        col(2)=region(1,2)
-        col(3)=region(1,3)
-        col(4)=region(2,1)
-        col(5)=region(2,2)
-        col(6)=region(2,3)
-        col(7)=region(3,1)
-        col(8)=region(3,2)
-        col(9)=region(3,3)
+        col(1) = region(1,1)
+        col(2) = region(1,2)
+        col(3) = region(1,3)
+        col(4) = region(2,1)
+        col(5) = region(2,2)
+        col(6) = region(2,3)
+        col(7) = region(3,1)
+        col(8) = region(3,2)
+        col(9) = region(3,3)
         if (ColonneOuLigneValide(col)) then
-            RegionValide=.true.
+            RegionValide = .true.
         else
-            RegionValide=.false.
+            RegionValide = .false.
         end if
     end function RegionValide
 
@@ -411,31 +412,31 @@ contains
         ! Variables locales :
         integer(1) :: l,c
 
-        GrilleValide=.true.
+        GrilleValide = .true.
 
         ! Vérification des lignes :
-        do l=1,9,+1
+        do l = 1,9,+1
             if (.not.ColonneOuLigneValide(g(l,1:9))) then
-                GrilleValide=.false.
+                GrilleValide = .false.
                 return
                 !print *, "Ligne ",l," non valide"
             end if
         end do
 
         ! Vérification des colonnes :
-        do c=1,9,+1
+        do c = 1,9,+1
             if (.not.ColonneOuLigneValide(g(1:9,c))) then
-                GrilleValide=.false.
+                GrilleValide = .false.
                 return
                 !print *, "Colonne ",c," non valide"
             end if
         end do
 
         ! Vérification des régions :
-        do l=1,7,+3
-            do c=1,7,+3
+        do l = 1,7,+3
+            do c = 1,7,+3
                 if (.not.RegionValide(g(l:l+2,c:c+2))) then
-                    GrilleValide=.false.
+                    GrilleValide = .false.
                     return
                     !print *, "Region ",l,c," non valide"
                 end if
@@ -453,19 +454,19 @@ contains
 
         integer(4) :: boucle , n
 
-        call date_and_time(VALUES=valeursTemps)
+        call date_and_time(VALUES = valeursTemps)
 
         ! On récupère le nombre d'entiers servant à stocker la graine :
-        call random_seed(SIZE=n)
+        call random_seed(SIZE = n)
         allocate(graine(1:n))
 
         ! On utilise les millièmes de seconde de l'horloge :
-        do boucle=1 , n
-            graine(boucle)=huge(graine(boucle))/1000*valeursTemps(8)
+        do boucle = 1 , n
+            graine(boucle) = huge(graine(boucle))/1000*valeursTemps(8)
         end do
 
         ! On transmet la graine :
-        call random_seed(put=graine(1:n))
+        call random_seed(put = graine(1:n))
     end subroutine Initialiser_Random
 
 
@@ -477,7 +478,7 @@ contains
         Real(8) :: t
 
         call cpu_time(t)
-        Temps=t
+        Temps = t
     end function Temps
 
 end module sudoku
