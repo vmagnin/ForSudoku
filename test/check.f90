8e55500b70e9f8c7c0a7d1aff8307787024f0aa1
Norwid Behrnd <nbehrnd@yahoo.com>
2023-08-28 21:47:31 +0200

    extend fpm test, reading decorated input files
    The reading of the input files including dashes and pipes to
    provide a visual orientation now equally is tested.
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- test/check.f90
+++ test/check.f90
@@ -1,10 +1,101 @@
 program check
-   use sudoku, only: ResoudreGrille
+   use sudoku, only: Lire_grille, ResoudreGrille
    implicit none
 
+   call assert_readtest01()
+   call assert_readtest02()
    call assert_wikipedia_solution()
 
 contains
+
+   subroutine assert_readtest01()
+   ! lecture d'une grille a compléter structurée, cases vides
+   integer :: grille_reference(9,9), grille_fichier(9,9)
+   integer :: i, j
+   logical :: array_equality
+
+   array_equality = .true.
+
+      grille_reference(:, 1) = [5, 3, 0,  0, 7, 0,  0, 0, 0]
+      grille_reference(:, 2) = [6, 0, 0,  1, 9, 5,  0, 0, 0]
+      grille_reference(:, 3) = [0, 9, 8,  0, 0, 0,  0, 6, 0]
+
+      grille_reference(:, 4) = [8, 0, 0,  0, 6, 0,  0, 0, 3]
+      grille_reference(:, 5) = [4, 0, 0,  8, 0, 3,  0, 0, 1]
+      grille_reference(:, 6) = [7, 0, 0,  0, 2, 0,  0, 0, 6]
+
+      grille_reference(:, 7) = [0, 6, 0,  0, 0, 0,  2, 8, 0]
+      grille_reference(:, 8) = [0, 0, 0,  4, 1, 9,  0, 0, 5]
+      grille_reference(:, 9) = [0, 0, 0,  0, 8, 0,  0, 7, 9]
+
+      call Lire_grille(grille_fichier, "./test/test_in_01.txt")
+      grille_fichier = transpose(grille_fichier)
+
+      outer: do i = 1, 9
+         ! write (*, "(9I3)") grille_fichier(:,i)
+         inner: do j = 1, 9
+            if (grille_reference(i, j) /= grille_fichier(i, j)) then
+               array_equality = .false.
+               print *, "À i : ", i, "j : ", j, &
+                  "grille_reference : ", grille_reference(i,j), &
+                  "n'est pas égale à grille_fichier :", grille_fichier(i,j)
+               exit outer
+           end if
+         end do inner
+      end do outer
+
+      if (array_equality .eqv. .false.) then
+         print *, "Reading check on `test_in_01.txt` failed."
+      else
+         print *, "Reading check on `test_in_01.txt` was successful."
+      end if
+   end subroutine assert_readtest01
+
+
+   subroutine assert_readtest02()
+   ! lecture d'une grille structurée à compléter, chaque case [0-9]
+   integer :: grille_reference(9,9), grille_fichier(9,9)
+   integer :: i, j
+   logical :: array_equality
+
+   array_equality = .true.
+
+      grille_reference(:, 1) = [5, 3, 0,  0, 7, 0,  0, 0, 0]
+      grille_reference(:, 2) = [6, 0, 0,  1, 9, 5,  0, 0, 0]
+      grille_reference(:, 3) = [0, 9, 8,  0, 0, 0,  0, 6, 0]
+
+      grille_reference(:, 4) = [8, 0, 0,  0, 6, 0,  0, 0, 3]
+      grille_reference(:, 5) = [4, 0, 0,  8, 0, 3,  0, 0, 1]
+      grille_reference(:, 6) = [7, 0, 0,  0, 2, 0,  0, 0, 6]
+
+      grille_reference(:, 7) = [0, 6, 0,  0, 0, 0,  2, 8, 0]
+      grille_reference(:, 8) = [0, 0, 0,  4, 1, 9,  0, 0, 5]
+      grille_reference(:, 9) = [0, 0, 0,  0, 8, 0,  0, 7, 9]
+
+      call Lire_grille(grille_fichier, "./test/test_in_02.txt")
+      grille_fichier = transpose(grille_fichier)
+
+      outer: do i = 1, 9
+         ! write (*, "(9I3)") grille_fichier(:,i)
+         inner: do j = 1, 9
+            if (grille_reference(i, j) /= grille_fichier(i, j)) then
+               array_equality = .false.
+               print *, "À i : ", i, "j : ", j, &
+                  "grille_reference : ", grille_reference(i,j), &
+                  "n'est pas égale à grille_fichier :", grille_fichier(i,j)
+               exit outer
+           end if
+         end do inner
+      end do outer
+
+
+      if (array_equality .eqv. .false.) then
+         print *, "Reading check on `test_in_02.txt` failed."
+      else
+         print *, "Reading check on `test_in_02.txt` was successful."
+      end if
+   end subroutine assert_readtest02
+
 
    subroutine assert_wikipedia_solution()
       ! voir : https://en.wikipedia.org/wiki/Sudoku
